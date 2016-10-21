dcf5b589d438f52d8513e30ceee7cc40fdcd7795
Cody Phillips <moarguru13@gmail.com>
2016-10-20 23:45:59 -0400

    Updating about.

--- client/about.js
+++ client/about.js
@@ -1,8 +1,37 @@
 about = {
   controller: function(){
     var ctrl = this;
+    ctrl.startTime = function() {
+
+      var today = new Date();
+      var h = today.getHours();
+      var m = today.getMinutes();
+      var s = today.getSeconds();
+      m = ctrl.checkTime(m);
+      s = ctrl.checkTime(s);
+      document.getElementById('txt').innerHTML =
+          h + ":" + m + ":" + s;
+      var t = setTimeout(ctrl.startTime, 500);
+    }
   },
-  view: function(ctrl){
-    return m('.jumbotron', 'About info')
-  }
+  view: function(ctrl){ return m('div', {class: "gadgetsContainer"},
+      [m('button', {class: "createBtn", id: "txt"}, "Drag"), m('button',{class: "createBtn", id: "txt"}, "and")],
+
+      [m('button', {class: "createBtn", id: "txt"}, "Drop"), m('button', {class: "createBtn", id: "txt"}, "me")]
+  ); }
 }
+$(function() {
+  $('body').on('mousedown', '#txt', function() {
+    $(this).addClass('draggable').parents().on('mousemove', function(e) {
+      $('.draggable').offset({
+        top: e.pageY - $('.draggable').outerHeight() / 2,
+        left: e.pageX - $('.draggable').outerWidth() / 2
+      }).on('mouseup', function() {
+        $(this).removeClass('draggable');
+      });
+    });
+    //e.preventDefault();
+  }).on('mouseup', function() {
+    $('.draggable').removeClass('draggable');
+  });
+});
